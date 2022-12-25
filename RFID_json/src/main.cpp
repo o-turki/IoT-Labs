c5e94b78d177c78233dff7de17825e4a0f6bdec9
Othman Turki <o.turki@advantryx.com>
2022-12-25 21:33:25 +0100

    Added Post Req

--- RFID_json/src/main.cpp
+++ RFID_json/src/main.cpp
@@ -21,21 +21,24 @@ const char *apiServer = "http://192.168.199.61/iot_lab/";
 
 void connect2Network();
 String httpGETRequest(String endPoint);
+String httpPOSTRequest(String endPoint, String data);
 
 String tagRFID = "";
 String endPoint = "";
 String studentJSON = "";
+String presenceData = "";
+String presenceDataJson = "";
 
 void setup()
 {
     Serial.begin(115200);
     pinMode(buzzer, OUTPUT);
 
-    connect2Network();
-
     SPI.begin();
     delay(100);
     mfrc522.PCD_Init();
+
+    connect2Network();
 }
 
 void loop()
@@ -57,14 +60,13 @@ void loop()
             {
                 tagRFID += (mfrc522.uid.uidByte[i] < 0x10 ? "0" : "") + String(mfrc522.uid.uidByte[i], HEX);
             }
-            Serial.println("> TAG RFID: " + tagRFID);
+            Serial.println("> tagRFID: " + tagRFID);
 
             endPoint = apiServer + String("?rfid=" + tagRFID);
             Serial.println("> endPoint: " + endPoint);
 
             studentJSON = httpGETRequest(endPoint);
-            Serial.println("> STUDENT JSON: " + studentJSON);
-            Serial.println(); // print an empty line
+            Serial.println("> studentJSON: " + studentJSON);
 
             if (studentJSON.length() > 4)
             {
@@ -80,15 +82,29 @@ void loop()
 
                 int id = studentDoc["id"];                         // 1
                 const char *rfid = studentDoc["rfid"];             // "539fac19"
-                const char *first_name = studentDoc["first_name"]; // "Othman"
-                const char *last_name = studentDoc["last_name"];   // "Turki"
+                const char *first_name = studentDoc["first_name"]; // "John"
+                const char *last_name = studentDoc["last_name"];   // "Doe"
 
                 Serial.println("================================================================================");
                 Serial.println("> Hello,");
                 Serial.println("> " + String(first_name) + " " + String(last_name));
-                Serial.println("================================================================================\n");
+                Serial.println("================================================================================");
+
+                // SERIALIZE DATA (Convert to JSON)
+                StaticJsonDocument<128> presenceDoc;
+                presenceDoc["first_name"] = String(first_name);
+                presenceDoc["last_name"] = String(last_name);
+                presenceData = "";
+                serializeJson(presenceDoc, presenceData);
+                Serial.println("> presenceData: " + presenceData);
+
+                // SEND A POST REQUEST WITH DATA
+                endPoint = apiServer;
+                presenceDataJson = httpPOSTRequest(endPoint, presenceData);
+                Serial.println("> presenceDataJson: " + presenceDataJson);
             }
 
+            Serial.println(); // print an empty line
             mfrc522.PICC_HaltA();
             mfrc522.PCD_StopCrypto1();
         }
@@ -119,6 +135,30 @@ String httpGETRequest(String endPoint)
     HTTPClient http;
     http.begin(endPoint);
     int httpResponseCode = http.GET();
+
+    String payload = "{}";
+
+    if (httpResponseCode > 0)
+    {
+        // Serial.print("HTTP Response code: ");
+        // Serial.println(httpResponseCode);
+        payload = http.getString();
+    }
+    else
+    {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+    }
+    http.end();
+    return payload;
+}
+
+String httpPOSTRequest(String endPoint, String data)
+{
+    HTTPClient http;
+    http.begin(endPoint);
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.POST(data);
 
     String payload = "{}";
 
